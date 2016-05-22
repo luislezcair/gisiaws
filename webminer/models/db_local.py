0949ce0624c2f2ce7521bfd881482af31b5c88a8
Luis Lezcano Airaldi <luislezcair@gmail.com>
2016-05-22 14:29:13 -0300

    Arreglar ruta al script webminer

--- webminer/models/db_local.py
+++
@@ -1,5 +0,0 @@
-# db = Database("sqlite", "database.sqlite", create_db=True)
-# db = Database("sqlite", "../../../gisiaws.sqlite3", create_db=True)
-def getDb():
-    db = Database('mysql', host='localhost', user='root', passwd='motocross1' , db="gisiaws")
-    return db
