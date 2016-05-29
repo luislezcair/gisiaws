27b65dc886f835337122538dacb2baa13073b530
Witzke <leanwitzke@gmail.com>
2016-05-29 18:26:53 -0300

    Fix intelligo

--- get_urls/Buscadores/google.py
+++ get_urls/Buscadores/google.py
@@ -15,7 +15,8 @@ def generar_consulta_google(q):
     reload(sys)
     sys.setdefaultencoding('utf8')
 
-    engine_google = Google(license="AIzaSyCvvHb8SYgHvS5gEIQabxuJ0Kl0sYdHl9U", language="en")       
+    # engine_google = Google(license="AIzaSyCvvHb8SYgHvS5gEIQabxuJ0Kl0sYdHl9U", language="en")
+    engine_google = Google(license="AIzaSyCKlCEJ41mE_6gqTN2AI9J4iSB-2L55zR0", language="en")       
     for consulta in q:
         for i in range(1, 2):
             for result in engine_google.search(consulta, start=i, count=10, type=SEARCH, cached=False):
--- get_urls/Buscadores/intelligo.py
+++ get_urls/Buscadores/intelligo.py
@@ -8,14 +8,26 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 from pattern.web import download
 from BeautifulSoup import BeautifulSoup
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+import time
 
 def generar_consulta_intelligo(consultas):
     urls = []
-    driver = webdriver.PhantomJS()
-    driver.set_script_timeout(10)
-    driver.maximize_window()
+    # Para arreglar problema de la variable twtrr
+    user_agent = (
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_4) " +
+    "AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.57 Safari/537.36"
+    )
+
+    dcap = dict(DesiredCapabilities.PHANTOMJS)
+    dcap["phantomjs.page.settings.userAgent"] = user_agent
+
+    driver = webdriver.PhantomJS(desired_capabilities=dcap)
+    driver.set_script_timeout(15)
+    driver.set_window_size(1280, 1024)
+    # driver.maximize_window()
     for consulta in consultas:
-        driver.get("http://patentes.explora-intelligo.info/index.html?setLng=es")
+        driver.get("http://patentes.explora-intelligo.info/index.html?setLng=en")
         ids = driver.find_element_by_id('btnCloseHelp')
         ids.click()
         driver.find_element_by_id('tbQuery').send_keys(consulta)
